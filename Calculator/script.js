e1937645f0798332066efb7089b17d96d1807d85
Sanketbhandari <sanketbhandari22@gmail.com>
2023-03-05 10:12:45 +0530

    updated Calculator code

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,3 +1,20 @@
-const button = document.querySelector("button");
-console.log(button)
-button.innerHTML = "hello";
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
